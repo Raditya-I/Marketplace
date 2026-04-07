89c514f90e0a9670dfdc8a84537e90869ffff352
Raditya-I <rafif54001@gmail.com>
2026-04-07 19:53:56 +0700

    update hind.js

--- hind.js
+++ hind.js
@@ -8,6 +8,7 @@ let elemen = document.getElementById("w");
 elemen.innerText= "Diperbarui " + formattedDate
 
 onload = function () {
+  document.getElementById("form").style.display="none";
   document.getElementById("profil").style.display = "none";
   document.getElementById("product").style.display = "none";
 }
@@ -15,19 +16,59 @@ document.getElementById("home").style.display = "block";
 
 document.getElementById("a1").onclick = function(e) {
   e.preventDefault();
+  document.getElementById("form").style.display="none";
   document.getElementById("home").style.display = "block";
   document.getElementById("profil").style.display = "none";
   document.getElementById("product").style.display = "none";
+  document.getElementById("k").style.display = "none";
+  document.getElementById("a1").style.borderBottom= "1px solid white";
+  document.getElementById("a2").style.borderBottom= "none";
+  document.getElementById("a3").style.borderBottom= "none";
 }
 document.getElementById("a2").onclick = function(e) {
   e.preventDefault();
+  document.getElementById("form").style.display="none";
   document.getElementById("home").style.display = "none";
   document.getElementById("profil").style.display = "block";
   document.getElementById("product").style.display = "none";
+  document.getElementById("k").style.display = "none";
+  document.getElementById("a1").style.borderBottom= "none"
+  document.getElementById("a2").style.borderBottom= "1px solid white";
+  document.getElementById("a3").style.borderBottom= "none";
 }
 document.getElementById("a3").onclick = function(e) {
   e.preventDefault();
+  document.getElementById("form").style.display="none";
   document.getElementById("home").style.display = "none";
   document.getElementById("profil").style.display = "none";
   document.getElementById("product").style.display = "block";
+  document.getElementById("k").style.display = "none";
+  document.getElementById("a1").style.borderBottom= "none";
+  document.getElementById("a2").style.borderBottom= "none";
+  document.getElementById("a3").style.borderBottom= "1px solid white";
 }
+function formm() {
+  document.getElementById("home").style.display="none";
+  document.getElementById("form").style.display ="block";
+}
+function konfr() {
+  const nama = document.getElementById("nama").value;
+  const kelas = document.getElementById("kelas").value;
+  const k = document.getElementById("k");
+  const hs = document.getElementById("hs");
+  const ht = document.getElementById("ht");
+  let Barudata = {nama, kelas}
+  if (nama === "") {
+    k.innerText= "NAMA TIDAK BOLEH KOSONG"
+  }
+  else if (kelas==="") {
+      k.innerText="KELAS TIDAK BOLEH KOSONG"
+    }
+    else {
+      k.innerText="SELAMAT DATANG!";
+      hs.innerText = "Nama : " + nama;
+      ht.innerText = "Kelas   : " + kelas;
+      localStorage.setItem("data", JSON.stringify(Barudata))
+    }
+  }
+  
